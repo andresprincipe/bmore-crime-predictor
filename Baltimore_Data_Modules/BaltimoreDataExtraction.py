6af403400a4851e26d2b179c1a719dd836099511
Andres Principe <andres.principe94@gmail.com>
2023-11-21 18:11:17 -0500

    Began integration of Offset API querying
    Added the ability to save multiple JSON files that encompass the entirety of the Baltimore crime dataset. Moved a class from the API utils into the baltimore data extraction .py

--- Baltimore_Data_Modules/BaltimoreDataExtraction.py
+++ Baltimore_Data_Modules/BaltimoreDataExtraction.py
@@ -8,7 +8,60 @@ import requests
 import json
 import os
 from datetime import date
+from urllib.parse import urlparse, urlunparse
 
+class APIOffset_UrlGather:
+    """
+    This class takes in an input of an API url taken from a given dataset on 
+    the OpenBaltimore website: https://data.baltimorecity.gov/ 
+    The functions of this class are able to modify the API urls and check for 
+    the number of features in a GET request response. Combining these 
+    functions allow you to programatically generate a list of working urls that
+    you can use to obtain all the data in a given Open Baltimore dataset.
+    """
+    def __init__(self,init_url_str):
+        self.url_ = init_url_str
+        self.url_parsed_ = urlparse(init_url_str)
+        self.query_ = self.url_parsed_.query
+        self.queriesli_ = self.query_.split('&')
+        
+    def AddOffsetToURL(self,offset):
+        initial_url = self.url_parsed_
+        offsetAddition = 'resultOffset={}'.format(offset)
+        paginationAddition = 'supportsPagination=true'
+        new_queries_li = [offsetAddition]+[paginationAddition]+self.queriesli_
+        new_query = '&'.join(new_queries_li)
+        new_url_parsed = initial_url._replace(query=new_query)
+        new_url = urlunparse(new_url_parsed)
+        return new_url
+    
+    def CheckFeatures(self,url_str):
+        response_num = requests.get(url_str).status_code
+        if response_num != 200:
+            print('HTTP code:',response_num,'no features')
+        else:
+            response_dict = requests.get(url_str).json()
+            if len(response_dict['features']) > 0:
+                return len(response_dict['features'])
+            else:
+                print('Request sucessful, but no features available')
+                return None
+            
+    def GatherOffsetUrls(self):
+        """
+        This function is used to programatically create all the urls necessary
+        to obtain the entirety of a given Open Baltimore dataset. Each request 
+        to Open Baltimore is limited to 2000 "rowIDs" as of 11/20/2023.
+        """
+        AllDataURLs_li = [self.url_]
+        feature_num = self.CheckFeatures(self.url_)
+        offset_num = 0
+        while feature_num is not None:
+            offset_num += 2000
+            url = self.AddOffsetToURL(offset_num)
+            feature_num = self.CheckFeatures(url)
+            AllDataURLs_li.append(url)
+        return AllDataURLs_li[:-1]
 
 
 class BmoreDataExtraction:
@@ -59,27 +112,50 @@ class BmoreDataExtraction:
             Bmore_data_path = cwd+'/'+existing_dir+'/Baltimore_Data/'
             return Bmore_data_path
     
-    def DownloadDataType(self,data_type_str,chosen_dir=None):
+    def DownloadDataType(self,data_type_str,includeAll,chosen_dir=None):
         HTTP_Status_dict, available_li = self.LinkCheck(data_type_str)
         clean_available_li = self.CleanDataNames(available_li)
         OGName_CleanName_dict = dict(zip(available_li,clean_available_li))
-        print('Downloading Available Baltimore Data...')
         if chosen_dir is None:
             path_str = self.createDataPath()
         if chosen_dir is not None:
             path_str = self.createDataPath(chosen_dir)
         if os.path.exists(path_str) is False:
             os.mkdir(path_str)
-        DataPath_dict = {}
-        for data_name in tqdm(available_li):
-            filename_str = OGName_CleanName_dict[data_name]+'_'+str(date.today())+'.json'
-            full_path_str = path_str+filename_str
-            r = requests.get(self.API_dict_[data_type_str][data_name])
-            with open(full_path_str, "w+") as f:
-                json.dump(r.json(), f)
-            DataPath_dict[data_name] = full_path_str
-        return DataPath_dict
-    
+        print('Downloading Available Baltimore Data...')
+        if includeAll is False:
+            DataPath_dict = {}
+            for data_name in tqdm(available_li):
+                filename_str = OGName_CleanName_dict[data_name]+'_'+str(date.today())+'.json'
+                full_path_str = path_str+filename_str
+                r = requests.get(self.API_dict_[data_type_str][data_name])
+                with open(full_path_str, "w+") as f:
+                    json.dump(r.json(), f)
+                DataPath_dict[data_name] = full_path_str
+            return DataPath_dict
+        elif includeAll is True:
+            DataPath_dict = {}
+            for data_name in available_li:
+                newSubPath_str = path_str+'/'+OGName_CleanName_dict[data_name]+'/'
+                if os.path.exists(newSubPath_str) is False:
+                    os.mkdir(newSubPath_str)
+                initial_url = self.API_dict_[data_type_str][data_name]
+                apio_ug = APIOffset_UrlGather(initial_url)
+                print('... gathering urls for all data GET requests ...')
+                full_url_li = apio_ug.GatherOffsetUrls()
+                requests_map = map(requests.get,full_url_li)
+                filenum = 0
+                subDataPath_dict = {}
+                for r in tqdm(requests_map):
+                    filenum += 1
+                    filename_str = OGName_CleanName_dict[data_name]+'_'+str(filenum)+'.json'
+                    full_path_str = newSubPath_str+filename_str
+                    with open(full_path_str, "w+") as f:
+                        json.dump(r.json(), f)
+                    subDataPath_dict[filenum] = full_path_str
+                DataPath_dict[data_name] = subDataPath_dict
+            return DataPath_dict
+                    
     def FeatureCollectionToDF(self,DataPath_dict):
         print('Turning Feature Collections data Into DataFrames...')
         FEAT_DATA_DF_dict = {}
