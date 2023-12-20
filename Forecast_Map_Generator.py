d987ace9db67ba1d3c370f0c7828950cb9a1edbc
Andres Principe <andres_principe@AndresHperdrive.fios-router.home>
2023-12-20 13:08:34 -0500

    Finalized HTML Map Output
    Successfully created a heatmap that has a layer of the predicted Day sin feature.

--- Forecast_Map_Generator.py
+++ Forecast_Map_Generator.py
@@ -7,36 +7,62 @@ Created on Mon Dec 18 15:22:06 2023
 """
 
 import pandas as pd
+from folium.plugins import HeatMap
 import folium
 import json
 import os
+import webbrowser
+from sklearn.preprocessing import MinMaxScaler
+
 
 metadata_filename = 'deploy_metadata.json'
 forecast_data_filename = os.getcwd()+'/Bmore_Crime_Forecast.csv'
 
-# Loading Crime Forecast Data
+## Loading Crime Forecast Data
 Bmore_Crime_Forecast_df = pd.read_csv(forecast_data_filename)
-Bmore_Crime_Forecast_df.index = Bmore_Crime_Forecast_df['Unnamed: 0']
-# Loading Metadata
+Bmore_Crime_Forecast_df.index = pd.to_datetime(Bmore_Crime_Forecast_df['Unnamed: 0'].rename('DATETIME'))
+Bmore_Crime_Forecast_df['date'] = Bmore_Crime_Forecast_df.index[0].date()
+Bmore_Crime_Forecast_df = Bmore_Crime_Forecast_df.drop(columns='Unnamed: 0')
+## Loading Metadata
 f = open(metadata_filename)
 metadata_dict = json.load(f)
 
-GEO_Crime_Forecast_df = Bmore_Crime_Forecast_df[metadata_dict['geo_cols_']]
-geo_forecast_li = GEO_Crime_Forecast_df.values.tolist()
+## Keeping only geolocation and scaling weights. Using Day sin as weights.
+## Using Day sin as weights gives the heatmap a sense of frequency at each 
+## predicted crime location
+mms = MinMaxScaler()
+mms.fit(Bmore_Crime_Forecast_df['Day sin'].values.reshape(-1, 1))
+GEO_Crime_Forecast_df = Bmore_Crime_Forecast_df[metadata_dict['geo_cols_']+['Day sin']]
+# GEO_Crime_Forecast_df = Bmore_Crime_Forecast_df[metadata_dict['geo_cols_']]
+scaled_weights = mms.transform(Bmore_Crime_Forecast_df['Day sin'].values.reshape(-1, 1))
+GEO_Crime_Forecast_df.loc[:,'Day sin'] = scaled_weights
+
+## Formatting data for map visualization
+GEO_data = [ list(array) for array in list(GEO_Crime_Forecast_df.values)]
 
 start_date = pd.to_datetime(GEO_Crime_Forecast_df.index[0]).date().strftime('%m-%d-%Y')
 end_date = pd.to_datetime(GEO_Crime_Forecast_df.index[-1]).date().strftime('%m-%d-%Y')
-timerange_str = start_date + '_to_' + end_date
+timerange_str = start_date + ' to ' + end_date
+layername = 'Daily Frequency from ' + timerange_str
 
-map_folium = folium.Map(location=[39.299236, -76.609383],
-                  tiles="OpenStreetMap",
-                  zoom_start=10)
+## Creating the name of the layer for the heatmap
+start_date = pd.to_datetime(GEO_Crime_Forecast_df.index[0]).date().strftime('%m-%d-%Y')
+end_date = pd.to_datetime(GEO_Crime_Forecast_df.index[-1]).date().strftime('%m-%d-%Y')
+timerange_str = start_date + ' to ' + end_date
+layername = 'Daily Frequency from ' + timerange_str
+## Generating the heat map and saving it externally for later use
+crime_forecast_map = folium.Map(location=[39.299236, -76.609383],
+                                tiles='OpenStreetMap',
+                                zoom_start=12)
 
-for coord_index in range(len(GEO_Crime_Forecast_df)):
-    pred_datetime_str = str(GEO_Crime_Forecast_df.iloc[coord_index].name)
-    coordinates_li = geo_forecast_li[coord_index]
-    map_folium.add_child(folium.Marker(location=coordinates_li,
-                          popup='Prediction Datetime:<br>'
-                          +pred_datetime_str))
-map_folium.save('ForecastMap'+timerange_str+'.html')
+HeatMap(GEO_data,max_opacity=0.5,name=layername).add_to(crime_forecast_map)
+
+html_file_str = 'ForecastMap.html'
+crime_forecast_map.save(html_file_str)
+html_browsr_url = 'file://' + os.getcwd() + '/' + html_file_str
+webbrowser.open(html_browsr_url, new=1)
+
+
+
+
 
